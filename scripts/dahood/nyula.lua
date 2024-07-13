813bda5da8bf409febcabdc98317074d460533a3
4Cafes <138708258+4Cafes@users.noreply.github.com>
2024-07-13 00:15:04 -0400

    Create nyula.lua

---
+++ scripts/dahood/nyula.lua
@@ -0,0 +1,4 @@
+-- THIS SCRIPT DOES NOT SUPPORT SOLARA
+-- Script provided by loadstring.lol 
+-- Website of cafes.lol
+loadstring(game:HttpGet("https://raw.githubusercontent.com/nyulachan/nyula/main/nyulauh"))();

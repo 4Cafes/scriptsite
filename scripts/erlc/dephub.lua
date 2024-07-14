c245728a67488d69756168066a7a3c47c620eb7c
4Cafes <138708258+4Cafes@users.noreply.github.com>
2024-07-14 16:05:28 -0400

    Create dephub.lua

---
+++ scripts/erlc/dephub.lua
@@ -0,0 +1,7 @@
+-- THIS SCRIPT WILL 100% GET THE ERLC ACCOUNT BANNED
+-- USE ON A ALT ONLY - SOLARA SUPPORTED
+-- Provided by loadstring.lol
+-- Website by cafes.lol
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DepHubHolder/DepHub1/main/Loader1"))()
+-- WAIT FOR ERLC TO FULLY LOAD BEFORE RUNNING - ONLY ESP AND SPEED
+-- TAKES A LONG MINUTE TO LOAD

0f1dd5e79d8f053af36ccd890716d979d8b56dd4
4Cafes <138708258+4Cafes@users.noreply.github.com>
2024-07-29 19:55:32 -0400

    Create infiniteyield.lua

---
+++ scripts/universal/infiniteyield.lua
@@ -0,0 +1,4 @@
+-- Provided by loadstring.lol
+-- A website by cafes.lol
+-- Script supports solara
+loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()

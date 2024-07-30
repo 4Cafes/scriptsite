08ba9d1b83017246fbb965d3a0676bb0679d0b1f
4Cafes <138708258+4Cafes@users.noreply.github.com>
2024-07-29 20:07:09 -0400

    Create aimmerzv2.lua

---
+++ scripts/universal/aimmerzv2.lua
@@ -0,0 +1,3 @@
+-- Script provided by loadstring.lol
+-- A website by cafes.lol
+loadstring(game:HttpGet('https://gist.githubusercontent.com/pleaseful/340594344eb73891941d2d01af742618/raw/94063ac38cbda5f382675ca949db75f6cc683fd8/Aimmerz%2520V2.lua'))()

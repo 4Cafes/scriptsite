0eac54b3c83e8d42e610dcc5c6d1304444010b25
4Cafes <138708258+4Cafes@users.noreply.github.com>
2024-07-31 21:47:27 -0400

    Create baconhub.lua

---
+++ scripts/beeswarm/baconhub.lua
@@ -0,0 +1,4 @@
+-- Provided by loadstring.lol
+-- A website of cafes.lol
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BaconBossScript/BeeSwarmSim/main/BeeSwarmSim"))()
+-- Has a key system with linkvertise

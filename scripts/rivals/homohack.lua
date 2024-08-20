91dac07a5be8826390ede1ffa0b0373743bb1085
4Cafes <138708258+4Cafes@users.noreply.github.com>
2024-08-20 12:58:00 -0400

    Update homohack.lua

--- scripts/rivals/homohack.lua
+++ scripts/rivals/homohack.lua
@@ -1,4 +1,4 @@
 --Script provided by loadstring.lol
 --A website of cafes.lol
---Does not work with wave or celery
+--Does not work with solara or celery
 loadstring(game:HttpGet("https://raw.githubusercontent.com/dementiaenjoyer/homohack/main/loader.lua"))()

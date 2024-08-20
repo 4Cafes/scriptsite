bde2bceff735d1c4af00b7144a76a89a3d634579
4Cafes <138708258+4Cafes@users.noreply.github.com>
2024-08-20 12:48:21 -0400

    Update zenith.lua

--- scripts/rivals/zenith.lua
+++ scripts/rivals/zenith.lua
@@ -1,5 +1,6 @@
 --Script provided by loadstring.lol
 --A website by cafes.lol
+--Solara and celery may not work
 Key = "PUT KEY IN HERE" --Get key from discord and put it in here : https://discord.gg/W7QQVUuX97
 SynZ = false --put true if your on synapse z
 loadstring(game:HttpGet("https://raw.githubusercontent.com/VaxKs/gfe/main/Rivalsss.lua"))()

59661d980b7f899e6591414a81e6ea0bbdba5904
4Cafes <138708258+4Cafes@users.noreply.github.com>
2024-08-20 17:57:39 -0400

    Create 6ftscripts.lua

---
+++ scripts/ps99/6ftscripts.lua
@@ -0,0 +1,4 @@
+-- Script provided by loadstring.lol
+-- A website of cafes.lol
+-- Only works on executors besides solara and celery (Untested)
+loadstring(game:HttpGet('https://raw.githubusercontent.com/SlamminPig/6FootScripts/main/Scripts/PetSimulator99.lua'))();

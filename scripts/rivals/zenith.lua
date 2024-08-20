9e05458a4cedd0ce1a18f82326c2edeaa461ca22
4Cafes <138708258+4Cafes@users.noreply.github.com>
2024-08-20 12:47:25 -0400

    Create zenith.lua

---
+++ scripts/rivals/zenith.lua
@@ -0,0 +1,5 @@
+--Script provided by loadstring.lol
+--A website by cafes.lol
+Key = "PUT KEY IN HERE" --Get key from discord and put it in here : https://discord.gg/W7QQVUuX97
+SynZ = false --put true if your on synapse z
+loadstring(game:HttpGet("https://raw.githubusercontent.com/VaxKs/gfe/main/Rivalsss.lua"))()
